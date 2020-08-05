13f598b87f5f6fc2beda3c2037eed6a108d8aeeb
Student_PSXGA136 <deablosf@hotmail.com>
2020-08-05 16:03:42 -0400

    one again

--- multiplyoutput.java
+++
@@ -1,8 +0,0 @@
-public class multiplyoutput{
-    public static void main(String[]args){
-        double meterConverter = 0.0254;
-        int inches = 25;
-        double inMeters = inches * meterConverter;
-        System.out.println(inches + " inches is about " + inMeters + " Meters");
-    }
-}
---
+++ unit conversion.java
@@ -0,0 +1,8 @@
+public class unit conversion{
+    public static void main(String[]args){
+        double meterConverter = 0.0254;
+        int inches = 25;
+        double inMeters = inches * meterConverter;
+        System.out.println(inches + " inches is about " + inMeters + " Meters");
+    }
+}
